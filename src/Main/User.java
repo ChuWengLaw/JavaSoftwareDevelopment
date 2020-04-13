db02315d7909ebb07038705cf6abca062bde66c5
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-04-13 16:44:33 +1000

    Create User.java

---
+++ src/Main/User.java
@@ -0,0 +1,26 @@
+
+public class User {
+    /* attributes
+    User ID
+    Password (encrypted)
+
+    Permissions
+        Create Billboards
+        List All Billboards
+        Edit All Billboards
+        Schedule Billboards
+        Edit Users
+    */
+//Default constructor
+    public User(){
+
+    }
+
+    //Constructor
+    public User(String UserName, String UserPassword, boolean CreateBillboards, boolean EditAllBillboard, boolean ScheduleBillboards, boolean EditUsers)
+    {
+
+    }
+
+}
+

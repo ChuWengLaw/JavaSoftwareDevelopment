e0ee0019370fd3714c4249875a9f25fc8edd91c8
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-04-17 13:23:54 +1000

    Update Billboard.java

--- src/Main/Billboard.java
+++ src/Main/Billboard.java
@@ -1,3 +1,9 @@
+package Main;//not sure if this is correct
+
+import java.util.Scanner;    //remove once gui is set up
+
+
+//double check method prefixes  (public/private/static ect)
 
 public class Billboard {
     /* attributes
@@ -7,6 +13,46 @@ public class Billboard {
     Colour of Text and background
     Images
     */
+
+    String BillboardName;
+    String CreatedByUserName;
+    String BillboardTextColour;
+    String BillboardBackgroundColour;
+    String BillboardMessage;
+    String BillboardPicture;
+    String BillboardInformation;
+
+//overloading constructors depending on info submitted.  OR
+    //One constructor but when you create a new billboard it inputs '0' to show that there was no input
+    //when you click create billboard you first enter a name and it checks if it exists
+
+    //Constructor
+    public Billboard(String BillboardName, String CreatedByUserName, String BillboardTextColour, String BillboardBackgroundColour, String BillboardMessage, String BillboardPicture, String BillboardInformation) {
+        this.BillboardName = BillboardName;
+        this.CreatedByUserName = CreatedByUserName;
+        this.BillboardTextColour = BillboardTextColour;
+        this.BillboardBackgroundColour = BillboardBackgroundColour;
+        this.BillboardMessage = BillboardMessage;
+        this.BillboardPicture = BillboardPicture;
+        this.BillboardInformation = BillboardInformation;
+    }
+
+
+    //use the GetCreateBillboardPermission() method
+    public String CreateBillboard(String BillBoardName)
+    {
+        //check if billboard already exists, if it doesnt create a new one.
+
+        //gui that gets the info you want it to store
+        //placeholder code
+        Scanner input = new Scanner(System.in);
+        String myString = input.next();
+
+        Billboard placeholderVariable = new Billboard(BillBoardName, )
+
+
+        return "sql code";
+    }
 
 }
 

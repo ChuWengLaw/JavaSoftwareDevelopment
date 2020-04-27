885c3d40c86c42f1a955e8de38232f74858d9a04
Lachlan <lachlangilbert2000@gmail.com>
2020-04-27 21:02:30 +1000

    Update CreateEditGUI.java

--- src/Main/CreateEditGUI.java
+++ src/Main/CreateEditGUI.java
@@ -67,7 +67,6 @@ public class CreateEditGUI extends JFrame implements ActionListener,Runnable{
                 } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
-                System.out.println(strBillboardName );
             }
         });
 

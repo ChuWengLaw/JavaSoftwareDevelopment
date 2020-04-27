d770027093f7f89f29ac5da310fd833ffe1d92d9
Eddy <carlos.law@outlook.com>
2020-04-27 12:48:22 +0800

    Database Connection and GUI actions by Law

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,13 @@
 package Main;
 
+import javax.swing.*;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        SwingUtilities.invokeLater(new CreateEditGUI("Create and Edit"));
+        Billboard bb = new Billboard();
+        bb.ConnectDatabase();
+        //TODO: Add action perform for CreateEditBillboard method when submit button is pressed
+    }
 }

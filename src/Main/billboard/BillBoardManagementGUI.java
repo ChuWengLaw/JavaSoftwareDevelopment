b4303cf1bac823f4b2ef36e9cda2fa2211536aff
Lachlan <lachlangilbert2000@gmail.com>
2020-05-11 17:25:04 +1000

    Update BillBoardManagementGUI.java

--- src/Main/billboard/BillBoardManagementGUI.java
+++ src/Main/billboard/BillBoardManagementGUI.java
@@ -10,6 +10,7 @@ public class BillBoardManagementGUI extends JFrame implements ActionListener,Run
     private JButton btnCreateEditBillboard;
     private JButton btnBillboardInfo;
     private JButton btnListBillboards;
+    private GridBagConstraints constraints = new GridBagConstraints();
 
     public BillBoardManagementGUI(String title) throws HeadlessException {
         super(title);
@@ -26,8 +27,59 @@ public class BillBoardManagementGUI extends JFrame implements ActionListener,Run
         btnCreateEditBillboard = createButton("Create or Edit Billboard");
         btnBillboardInfo = createButton("Billboard Info");
         btnListBillboards = createButton("List existing Billboards");
+
+        //create an actionListener for btnCreateEditBillboard
+        btnCreateEditBillboard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //create an actionListener for btnBillboardInfo
+        btnBillboardInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //create an actionListener for btnListBillboards
+        btnListBillboards.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JPanel menu = new JPanel(new GridBagLayout());
+
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10,10,10,10);
+
+        constraints.gridx=0;
+        constraints.gridy=0;
+        menu.add(btnCreateEditBillboard, constraints);
+
+        constraints.gridx =1;
+        menu.add(btnBillboardInfo,constraints);
+
+        constraints.gridx=2;
+        menu.add(btnListBillboards, constraints);
+
+        getContentPane().add(menu);
+
+
+        repaint();
+        setVisible(true);
     }
 
+    /**
+     * Create the buttons
+     * @author Lachlan
+     * @param text the text to be written on the button
+     * @return the configured button
+     */
     private JButton createButton(String text) {
         JButton button = new JButton();
         button.setText(text);

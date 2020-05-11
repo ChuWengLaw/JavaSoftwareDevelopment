b3e5637cbfe642a1f9f5e17641f868a1ae9ddbe5
Lachlan <lachlangilbert2000@gmail.com>
2020-05-11 19:18:26 +1000

    Update ListBillboardsGUI.java

--- src/Main/billboard/ListBillboardsGUI.java
+++ src/Main/billboard/ListBillboardsGUI.java
@@ -8,21 +8,22 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 import java.sql.SQLException;
 
-public class ListBillboardsGUI extends JFrame implements ActionListener, Runnable {
+public class ListBillboardsGUI extends JFrame {
 
     //set the width and height of the GUI
     public static final int HEIGHT = 300;
     public static final int WIDTH = 600;
 
 
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public ListBillboardsGUI() throws HeadlessException {
+        super("List Billboards");
+        createGUI();
     }
 
-    @Override
-    public void run() {
+    private void createGUI() {
+        setSize(WIDTH, HEIGHT);
 
+        repaint();
+        setVisible(true);
     }
 }

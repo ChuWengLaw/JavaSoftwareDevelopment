538c602f2f6997fc1a8f1d8309137ee01b720629
Lachlan <lachlangilbert2000@gmail.com>
2020-05-24 12:53:24 +1000

    have got the values from the xml being used for the billboard

--- src/Viewer/BillBoardViewer.java
+++ src/Viewer/BillBoardViewer.java
@@ -1,27 +1,46 @@
 package Viewer;
 
+import ControlPanel.billboard.ExractFromXML;
+
 import javax.imageio.ImageIO;
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
 import javax.swing.*;
+import javax.swing.colorchooser.ColorChooserComponentFactory;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.concurrent.*;
-
 
 /**
  * Billboard Viewer which connects to billboard server every
  * 15 seconds and display the billboards on screen
+ *
  * @author Law
  */
 public class BillBoardViewer extends JFrame {
     private JLabel picLabel = new JLabel();
     private JPanel panel = new JPanel();
 
+    ExractFromXML exractFromXML = new ExractFromXML("starWars");
+
+    private String message = exractFromXML.message;
+    private Color textColour = exractFromXML.textColour;
+    private String info = exractFromXML.information;
+    private Color infoColour = exractFromXML.informationColour;
+    private Color background = exractFromXML.backgroundColour;
+    private String imageAdress = exractFromXML.image;
+
+    private JLabel lblMessage;
+    private JLabel lblInfo;
+    private JPanel pnlBackground = new JPanel();
+    private JPanel pnlImage;
+
     public BillBoardViewer() throws HeadlessException, IOException {
         super("Billboard Viewer");
 
         // Create frame
-        setSize(500,400);
+        setSize(500, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -41,21 +60,47 @@ public class BillBoardViewer extends JFrame {
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         executor.scheduleAtFixedRate(fifteenSec, 0, 3, TimeUnit.SECONDS);
-}
+    }
 
     private void update() throws IOException {
         Image myPicture;
-        try{
+        try {
             myPicture = ImageIO.read(new File("src/Viewer/Image/soyeon-idle-pfantaken-Favim.com-6382977.jpg"));
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             //TODO: Display Error Screen
             myPicture = ImageIO.read(new File("src/Viewer/Image/soyeon-idle-pfantaken-Favim.com-6382977.jpg"));
         }
 
+        if (!message.isBlank() && info.isBlank() && imageAdress.isBlank()) {
+
+        } else if (message.isBlank() && info.isBlank() && !imageAdress.isBlank()) {
+
+        } else if (message.isBlank() && !info.isBlank() && imageAdress.isBlank()) {
+
+        } else if(!message.isBlank() && info.isBlank() && !imageAdress.isBlank()){
+
+        }
+        else if(!message.isBlank() && !info.isBlank()&& imageAdress.isBlank()){
+
+        }
+        else if(message.isBlank() && !info.isBlank()&& !imageAdress.isBlank()){
+
+        }
+        else{
+            
+        }
+
+
+        lblInfo = new JLabel(info);
+        lblInfo.setForeground(textColour);
+        lblMessage = new JLabel(message);
+        lblMessage.setForeground(infoColour);
+        pnlBackground.setBackground(background);
         picLabel.setIcon(new ImageIcon(myPicture));
         panel.setLayout(new BorderLayout());
-        panel.add(picLabel, BorderLayout.CENTER);
+        panel.add(pnlBackground, BorderLayout.CENTER);
+        panel.add(lblInfo, BorderLayout.NORTH);
+        panel.add(lblMessage, BorderLayout.SOUTH);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         getContentPane().add(panel);
         pack();

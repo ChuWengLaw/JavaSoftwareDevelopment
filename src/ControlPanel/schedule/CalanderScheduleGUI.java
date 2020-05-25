669f143f53a78749ca9533bcd9ece787d2c0a474
Callum <62454246+Colonel-Kentucky@users.noreply.github.com>
2020-05-25 16:39:17 +1000

    Revert "Update CalanderScheduleGUI.java"
    This reverts commit 219184a8bb8728d564a5089178fbbecdb362bd60.

--- src/ControlPanel/schedule/CalanderScheduleGUI.java
+++ src/ControlPanel/schedule/CalanderScheduleGUI.java
@@ -34,24 +34,12 @@ public class CalanderScheduleGUI extends JFrame {
     private JLabel lbl6;
     private JLabel lbl7;
 
+
     public CalanderScheduleGUI(){
         super("Scheduled Billboards");
         createGUI();
     }
     private void createGUI() {
-
-        int Min_in_Millis = 60000;
-        int Hour_in_Millis = 60*Min_in_Millis;
-        int Day_in_Millis = 24*Hour_in_Millis;
-
-        long CurrentTimeMillis = System.currentTimeMillis();
-        lbl1 = createLabel(CurrentTimeMillis);
-        lbl2 = createLabel(CurrentTimeMillis + 1*Day_in_Millis);
-        lbl3 = createLabel(CurrentTimeMillis + 2*Day_in_Millis);
-        lbl4 = createLabel(CurrentTimeMillis + 3*Day_in_Millis);
-        lbl5 = createLabel(CurrentTimeMillis + 4*Day_in_Millis);
-        lbl6 = createLabel(CurrentTimeMillis + 5*Day_in_Millis);
-        lbl7 = createLabel(CurrentTimeMillis + 6*Day_in_Millis);
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         btnBillboardScheduler.setText("Schedule a Billboard");
@@ -79,20 +67,39 @@ public class CalanderScheduleGUI extends JFrame {
         catch (Exception ex){
             ex.printStackTrace();
         }
-        ArrayList<String[]> ScheduleArray = Client.getScheduleArray();
 
-        System.out.println(ScheduleArray.get(0)[0]);
-        lbl1 = AddInfoToLabel(ScheduleArray, lbl1,CurrentTimeMillis + 0*Day_in_Millis);
-        lbl2 = AddInfoToLabel(ScheduleArray, lbl2,CurrentTimeMillis + 1*Day_in_Millis);
-        lbl3 = AddInfoToLabel(ScheduleArray, lbl3,CurrentTimeMillis + 2*Day_in_Millis);
-        lbl4 = AddInfoToLabel(ScheduleArray, lbl4,CurrentTimeMillis + 3*Day_in_Millis);
-        lbl5 = AddInfoToLabel(ScheduleArray, lbl5,CurrentTimeMillis + 4*Day_in_Millis);
-        lbl6 = AddInfoToLabel(ScheduleArray, lbl6,CurrentTimeMillis + 5*Day_in_Millis);
-        lbl7 = AddInfoToLabel(ScheduleArray, lbl7,CurrentTimeMillis + 6*Day_in_Millis);
+        //super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //JPanel panel = new JPanel();
+        //JScrollPane scrollpane = new JScrollPane(Client.getScheduleTable());
 
-        //...
+        ArrayList<String[]> testing = Client.getScheduleArray();
+        System.out.println(testing.get(0)[0]);
+
+        //panel.setLayout(new BorderLayout());
+        constraints.gridy = 10;
+        constraints.gridx = 10;
+        //pnlScheduleMenu.add(scrollpane, constraints);
         //###########################list billboards change to list scheduled
 
+
+
+
+        //GET SQL DATA ABOUT SCHEDULES Request from database
+        int Min_in_Millis = 60000;
+        int Hour_in_Millis = 60*Min_in_Millis;
+        int Day_in_Millis = 24*Hour_in_Millis;
+
+        long CurrentTimeMillis = System.currentTimeMillis();
+
+        lbl1 = createLabel(CurrentTimeMillis);
+        lbl2 = createLabel(CurrentTimeMillis + 1*Day_in_Millis);
+        lbl3 = createLabel(CurrentTimeMillis + 2*Day_in_Millis);
+        lbl4 = createLabel(CurrentTimeMillis + 3*Day_in_Millis);
+        lbl5 = createLabel(CurrentTimeMillis + 4*Day_in_Millis);
+        lbl6 = createLabel(CurrentTimeMillis + 5*Day_in_Millis);
+        lbl7 = createLabel(CurrentTimeMillis + 6*Day_in_Millis);
+
+        //Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
         int Buffer = 10;
 
         EmptyBorder border = new EmptyBorder(Buffer,Buffer,Buffer,Buffer);
@@ -106,7 +113,7 @@ public class CalanderScheduleGUI extends JFrame {
         lbl6.setBorder(border);
         lbl7.setBorder(border);
 
-        constraints.anchor = GridBagConstraints.NORTHWEST;
+        constraints.anchor = GridBagConstraints.WEST;
         constraints.insets = new Insets(10, 10, 10, 10);
 
         constraints.gridx = 0;
@@ -115,22 +122,22 @@ public class CalanderScheduleGUI extends JFrame {
         constraints.gridx = 0;
         constraints.gridy = 1;
         pnlScheduleMenu.add(btnDeleteSchedule, constraints);
-        constraints.gridy = 0;
-        constraints.gridx = 1;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
 
-        pnlScheduleMenu.add(lbl1, constraints);
-        constraints.gridx = 2;
-        pnlScheduleMenu.add(lbl2, constraints);
-        constraints.gridx = 3;
-        pnlScheduleMenu.add(lbl3, constraints);
-        constraints.gridx = 4;
-        pnlScheduleMenu.add(lbl4, constraints);
-        constraints.gridx = 5;
-        pnlScheduleMenu.add(lbl5, constraints);
-        constraints.gridx = 6;
-        pnlScheduleMenu.add(lbl6, constraints);
-        constraints.gridx = 7;
-        pnlScheduleMenu.add(lbl7, constraints);
+        pnlScheduleMenu.add(lbl1);
+        constraints.gridy = 2;
+        pnlScheduleMenu.add(lbl2);
+        constraints.gridy = 3;
+        pnlScheduleMenu.add(lbl3);
+        constraints.gridy = 4;
+        pnlScheduleMenu.add(lbl4);
+        constraints.gridy = 5;
+        pnlScheduleMenu.add(lbl5);
+        constraints.gridy = 6;
+        pnlScheduleMenu.add(lbl6);
+        constraints.gridy = 7;
+        pnlScheduleMenu.add(lbl7);
 
         getContentPane().add(pnlScheduleMenu);
         // Display the window
@@ -140,32 +147,11 @@ public class CalanderScheduleGUI extends JFrame {
         setVisible(true);
     }
 
-    private JLabel AddInfoToLabel(ArrayList<String[]> scheduleArray, JLabel lblIn, long Day_Start_Millis) {
-        SimpleDateFormat start_formatter= new SimpleDateFormat("yyyy-MM-dd 00:00:00");
-        SimpleDateFormat end_formatter= new SimpleDateFormat("yyyy-MM-dd 23:59:59");
-        Date DateStart = new Date(Day_Start_Millis);
-        String StringStart = start_formatter.format(DateStart);
-        String StringEnd = end_formatter.format(DateStart);
-
-        for (int i = 0; i < scheduleArray.size(); i++)
-        {
-            int IntStart = scheduleArray.get(i)[1].compareTo(StringStart);
-            int IntEnd = scheduleArray.get(i)[1].compareTo(StringEnd);
-            if (IntStart > 0 && IntEnd < 0)
-            {
-                lblIn.setText(lblIn.getText() +"<br/>"+ scheduleArray.get(i)[0] + " " + scheduleArray.get(i)[1]);
-            }
-        }
-        lblIn.setText(lblIn.getText() +"</html>");
-        return lblIn;
-    }
-
     private JLabel createLabel(long millis) {
-
         Date datePrint = new Date(millis);
         SimpleDateFormat formatter= new SimpleDateFormat("dd MMMM E");
         JLabel label = new JLabel();
-        label.setText("<html>" + formatter.format(datePrint));
+        label.setText(formatter.format(datePrint));
         return label;
     }
 }
